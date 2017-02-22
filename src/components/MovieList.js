2c0b4b051035a4a449ebb289c86b32415db59753
Vaibahv Mule <vaibhavmule135@gmail.com>
2017-02-23 03:20:42 +0530

    fix movielist

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,8 +5,9 @@ import { ScrollView, TextInput, View, Text } from 'react-native';
 class MovieList extends Component {
   constructor(props) {
     super(props);
+
     this.handleSearchText = this.handleSearchText.bind(this);
-    this.state = { movies: [], searchText: ''};
+    this.state = {loaded:false,movies: [], searchText: '', error: ''};
   }
 
 	renderMovies() {
@@ -14,17 +15,29 @@ class MovieList extends Component {
 			<Text key={movie.imdbID}>{movie.Title}</Text>)
 	}
 
+	renderLoading() {
+		if (this.state.error) {
+			return <Text>{this.state.error}</Text>
+		} else {
+			return <Text>Laoding..</Text>
+		}
+		
+	}
+
 	handleSearchText(searchText) {
 		this.setState({searchText})
+		this.setState({loaded:false})
 
 		fetch('http://www.omdbapi.com/?s=' + searchText)
 	    .then((response) => response.json())
-	    .then((responseData) => this.setState({ movies: responseData['Search'] }))
-	    .catch((error) => {
-				console.log('There has been a problem with your fetch operation: ' + error.message);
-				 // ADD THIS THROW error
-				  throw error;
-			});
+	    .then((responseData) => {
+	    	const error = responseData['Error']
+	    	if (error) {
+	    		this.setState({ loaded:false, movies: [], error: error })
+	    	} else {
+	    		this.setState({ loaded:true, movies: responseData['Search'], error: ''})
+	    	}
+	  	})
 	}
 
 	render() {
@@ -37,7 +50,7 @@ class MovieList extends Component {
 	        autoFocus={true}
 	      />
 	      <ScrollView>
-	      	{this.renderMovies()}
+	      	{this.state.loaded ? this.renderMovies(): this.renderLoading()}
 	      </ScrollView>
 			</View>
 		)
